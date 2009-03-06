cdd52dbae347b115a735d739f9680e25a0c349b4
Nick Dunn <nickdunn@nick-dunn-2.local>
2009-03-06 12:37:00 +0000

    Added support for partial matches

--- assets/filters.js
+++ assets/filters.js
@@ -3,9 +3,19 @@ $(document).ready(function() {
 	var matches = location.href.match(/\?filter=(([^:]+):(.*))?/);
 	var field = ''; var value = '';
 	
+	var regex = false;
+	var regex_prefix = 'regexp';
+	var comparison_options = '';
+	var comparisons = ['is', 'contains'];
+	
 	if (matches && matches[3] != undefined) {
 		field = unescape(matches[2]);
 		value = unescape(matches[3]);
+	}
+	
+	if (value.indexOf(regex_prefix) != -1) {
+		regex = true;
+		value = value.replace(/regexp:/,'');
 	}
 	
 	for (var item in filters) {
@@ -13,19 +23,28 @@ $(document).ready(function() {
 		
 		if (field == filters[item]) selected = ' selected="selected"';
 		
-		options = options + '<option' + selected + ' value="' + filters[item] + '">' + item + '</option>';
+		options += '<option' + selected + ' value="' + filters[item] + '">' + item + '</option>';
+	}	
+	
+	for (var i = 0; i < comparisons.length; i++) {
+		var selected = '';
+		
+		if (comparisons[i] == 'contains' && regex) selected = ' selected="selected"';		
+		
+		comparison_options += '<option' + selected + '>' + comparisons[i] + '</option>';
 	}
 	
 	$('h2').after('\
 		<form class="filters" method="POST" action="">\
 			<select class="field" name="field">' + options + '</select>\
+			<select class="match" name="match">' + comparison_options + '</select>\
 			<input class="value" name="value" value="' + value + '" />\
 			<input class="apply" type="submit" value="' + filters_apply + '" />\
 			<input class="clear" type="button" value="' + filters_clear + '" />\
 		</form>\
 	');
 	
-	$('.filters .field').change(function() {
+	$('.filters select').change(function() {
 		$('.filters .value').focus();
 	});
 	
@@ -39,9 +58,12 @@ $(document).ready(function() {
 		var self = $(this);
 		var field = self.find('.field').val();
 		var value = self.find('.value').val();
-		
+
 		if (field && value) {
-			location.href = '?filter=' + escape(field) + ':' + escape(value);
+			var href = '?filter=' + escape(field) + ':';
+			if (self.find('.match').val() == 'contains') href += regex_prefix + ':';
+			href += escape(value);
+			location.href = href;
 		}
 		
 		return false;
