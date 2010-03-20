fa02d5ecf44aaa9c75f3290edb299cdbe1965b71
Nick Dunn <nick@nick-dunn.local>
2010-03-20 14:03:32 +0000

    Added German translation

---
+++ lang/lang.de.php
@@ -0,0 +1,36 @@
+<?php
+
+	$about = array(
+		'name' => 'Deutsch',
+		'author' => array(
+			'name' => 'Nils Hörrmann',
+			'email' => 'post@nilshoerrmann.de',
+			'website' => 'http://www.nilshoerrmann.de'
+		),
+		'release-date' => '2010-03-19'
+	);
+	
+	
+	/*
+	 * EXTENSION: Publish Filtering
+	 * Localisation strings
+	 */
+
+	$dictionary = array(
+	
+		'Clear' =>
+		'Löschen',
+		
+		'contains' =>
+		'enthält',
+		
+		'is' =>
+		'entspricht',
+		
+		'entry' =>
+		'Eintrag',
+		
+		'entries' =>
+		'Einträge'
+
+	);
