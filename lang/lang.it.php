f4993bd36eea40df1fa5222be093ee00cb725537
Simone Economo <my.ekoes@gmail.com>
2010-08-25 01:27:25 +0800

    Italian localisation

---
+++ lang/lang.it.php
@@ -0,0 +1,36 @@
+<?php
+
+	$about = array(
+		'name' => 'Italiano',
+		'author' => array(
+			'name' => 'Simone Economo',
+			'email' => 'my.ekoes@gmail.com',
+			'website' => 'http://lineheight.net',
+		),
+		'release-date' => '2010-08-24',
+	);
+	
+	
+	/*
+	 * EXTENSION: Publish Filtering
+	 * Localisation strings
+	 */
+
+	$dictionary = array(
+	
+		'Clear' =>
+		'Azzera',
+		
+		'contains' =>
+		'contiene',
+		
+		'is' =>
+		'&egrave;',
+		
+		'entry' =>
+		'voce',
+		
+		'entries' =>
+		'voci'
+
+	);
