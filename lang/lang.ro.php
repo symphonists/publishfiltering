dd05f01691272981ae57c999d83c78f1544eb213
Vlad <vlad_micutul@yahoo.com>
2011-04-01 23:32:17 +0800

    Added Romanian translation.

---
+++ lang/lang.ro.php
@@ -0,0 +1,39 @@
+<?php
+
+	$about = array(
+		'name' => 'Română',
+		'author' => array(
+			'name' => 'Vlad Ghita',
+			'email' => 'vlad.ghita@xandergroup.ro',
+			'website' => ''
+		),
+		'release-date' => '2011-04-01'
+	);
+
+	/**
+	 * Publish Filtering
+	 */
+	$dictionary = array(
+
+		'Disable publish filtering for this section' => 
+		'Afişează filtrarea înregistrărilor pentru această secţiune.',
+
+		'contains' => 
+		'conţine',
+
+		'is' => 
+		'este',
+
+		'Filter' => 
+		'Filtrează',
+
+		'Clear filters' => 
+		'Şterge filtrele',
+
+		'entry' => 
+		'înregistrare',
+
+		'entries' => 
+		'înregistrări',
+
+	);
