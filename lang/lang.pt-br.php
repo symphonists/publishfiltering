723723a8063288e8a791aea94bea3804926fc226
Marcio Toledo <mt@marciotoledo.com>
2012-12-10 12:27:46 -0200

    Portuguese (Brazil) translation.

---
+++ lang/lang.pt-br.php
@@ -0,0 +1,39 @@
+<?php
+
+	$about = array(
+		'name' => 'Portuguese (Brazil)',
+		'author' => array(
+			'name' => 'Marcio Toledo',
+			'email' => 'mt@marciotoledo.com',
+			'website' => 'http://marciotoledo.com'
+		),
+		'release-date' => '2012-12-10'
+	);
+
+	/**
+	 * Publish Filtering
+	 */
+	$dictionary = array(
+
+		'contains' => 
+		'contém',
+
+		'entries' => 
+		'registros',
+
+		'entry' => 
+		'registro',
+
+		'is' => 
+		'é',
+
+		'Filter' => 
+		'Filtrar',
+
+		'Clear filters' => 
+		'Limpar filtros',
+		
+		'Disable publish filtering for this section' =>
+		'Desabilitar filtros de publicação para esta seção'
+
+	);
