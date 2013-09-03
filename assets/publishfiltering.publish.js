7e3f3e41783e23b2cd0f3ba027aa23455a257379
Nils Hörrmann <post@nilshoerrmann.de>
2013-09-03 12:06:47 +0200

    Dynamic, AJAX based filtering

--- assets/publishfiltering.publish.js
+++ assets/publishfiltering.publish.js
@@ -1,35 +1,216 @@
 (function($) {
 
+	Symphony.Language.add({
+		'Click to select': false,
+		'Type to search': false,
+		'You are looking at a filtered entry index.': false,
+		'Clear?': false
+	});
+
 	$(document).on('ready.publishfiltering', function() {
-		var publishfiltering = $('#drawer-publishfiltering'),
-			fields = publishfiltering.find('.publishfiltering-fields'),
-			comparison = publishfiltering.find('.publishfiltering-comparison'),
-			search = publishfiltering.find('.publishfiltering-search'),
-			options = Symphony.Context.get('publishfiltering'),
-			selectize;
 
-		// System messages
+		Publishfiltering = function() {
+			var contents = $('#contents'),
+				notifier = $('div.notifier'),
+				button = $('a[href="#drawer-publishfiltering"]'),
+				drawer = $('#drawer-publishfiltering'),
+				fields = drawer.find('.publishfiltering-fields'),
+				comparison = drawer.find('.publishfiltering-comparison'),
+				search = drawer.find('.publishfiltering-search'),
+				options = Symphony.Context.get('publishfiltering'),
+				comparisonSelectize, searchSelectize, fieldsSelectize;
 
+			var init = function() {
+				fields.selectize().on('change', switchField);
+				comparison.selectize().on('change', searchEntries);
+				search.selectize({
+					create: true,
+					maxItems: 1,
+					render: {
+        				option_create: searchPreview
+        			}
+				}).on('change', searchEntries);
 
-		// Search
-		search.selectize({
-			// create: true,
-			maxItems: 1
-		});
-		selectize = search[0].selectize;
+				// Store Selectize instances
+				fieldsSelectize = fields[0].selectize;
+				comparisonSelectize = comparison[0].selectize;
+				searchSelectize = search[0].selectize;
 
-		// Fields
-		fields.selectize({
-			onChange: function(value) {
-				selectize.clearOptions();
-				$.each(options[value], function(key, option) {
-					selectize.addOption({value: key, text: option});
-				})
+				// Restore filter from URL
+				restoreFilter();
+
+				// Notify about filtering
+				button.on('init.publishfiltering click.publishfiltering', notifyFilter).trigger('init.publishfiltering');
+			};
+
+			var restoreFilter = function() {
+				if(location.search.indexOf('filter') !== -1) {
+					var filter = location.search.substring(1).split('&');
+					$.each(filter, function(key, value) {
+						if(value.indexOf('filter') == 0) {
+							value = decodeURIComponent(value);
+							settings = value.match(/filter\[(.*)\]=(regexp:)?(.*)/);
+							fieldsSelectize.setValue(settings[1]);
+
+							// Add existing options to selection 
+							if(settings[2] == 'regexp:') {
+								searchSelectize.addOption({
+									value: settings[3],
+									text: settings[3]
+								});
+							}
+
+							searchSelectize.setValue(settings[3]);
+						}
+					});
+				}
+			};
+
+			var switchField = function(event) {
+				var field = fieldsSelectize.getValue();
+
+				// Clear
+				searchSelectize.clearOptions();
+
+				// Default options
+				if(hasOptions(field)) {
+					comparisonSelectize.setValue('is');
+					searchSelectize.$control_input.attr('placeholder', Symphony.Language.get('Click to select') + '…');
+					$.each(options[field], function(key, option) {
+						value = (typeof key === 'string') ? key : option;
+
+						searchSelectize.addOption({
+							value: value,
+							text: option
+						});
+					});
+				}
+
+				// Text search
+				else {
+					comparisonSelectize.setValue('contains');
+					searchSelectize.$control_input.attr('placeholder', Symphony.Language.get('Type to search') + '…');
+				}		
+			};
+
+			var searchPreview = function(item) {
+				return '<div class="create"><em>' + Symphony.Language.get('Search for') + ' ' + item.input + ' …</em></div>';
+			};
+
+			var searchEntries = function(event) {
+				var field = fieldsSelectize.getValue(),
+					comparison = comparisonSelectize.getValue(),
+					needle = searchSelectize.getValue(),
+					method, url;
+
+				// Fetch entries
+				if(field && needle) {
+					base = location.href.replace(location.search, '');
+					method = (comparison == 'contains') ? 'regexp:' : '';
+					url = base + '?filter[' + encodeURI(field) + ']=' + method + encodeURI(needle);
+
+					fetchEntries(url);
+					setURL(url);
+				}
+			};
+
+			var fetchEntries = function(url) {
+				$.ajax({
+					url: url,
+					dataType: 'html',
+					success: appendEntries
+				});
+			};
+
+			var appendEntries = function(result) {
+				var page = $(result),
+					entries = page.find('tbody'),
+					pagination = page.find('ul.page'),
+					pageform = page.find('.paginationform'),
+					pagegoto = pageform.find('input'),
+					pageactive = pagegoto.attr('data-active'),
+					pageinactive = pagegoto.attr('data-inactive');
+
+				// Update entry table
+				contents.find('tbody').replaceWith(entries);
+
+				// Update pagination, see symphony/assets/js/admin.js
+				contents.find('ul.page').remove();
+				contents.append(pagination);
+				pagegoto
+					.val(pageinactive)
+					.on('focus.admin', function(event) {
+						if(pagegoto.val() == pageactive) {
+							pagegoto.val('');
+						}
+						pageform.addClass('active');
+					})
+					.on('blur.admin', function(event) {
+						if(pageform.is('.invalid') || pagegoto.val() == '') {
+							pageform.removeClass('invalid');
+							pagegoto.val(pageinactive);
+						}
+						if(pagegoto.val() == pageinactive) {
+							pageform.removeClass('active');
+						}
+					}
+				);
+				pageform
+					.attr('action', window.location.href)
+					.on('mouseover.admin', function(event) {
+						if(!pageform.is('.active') && pagegoto.val() == pageinactive) {
+							pagegoto.val(pageactive);
+						}
+					})
+					.on('mouseout.admin', function(event) {
+						if(!pageform.is('.active') && pagegoto.val() == pageactive) {
+							pagegoto.val(pageinactive);
+						}
+					})
+					.on('submit.admin', function(event) {
+						if(parseInt(pagegoto.val()) > parseInt(pagegoto.attr('data-max'))) {
+							pageform.addClass('invalid');
+							return false;
+						}
+					}
+				);
 			}
-		});
 
-		// Comparison
-		comparison.selectize();
+			var setURL = function(url) {
+				if(!!(window.history && history.pushState)) {
+					history.pushState(null, null, url);
+				}
+			};
+
+			var clear = function() {
+				window.location.href = location.href.replace(location.search, '');
+			};
+
+			var hasOptions = function(name) {
+				return name in options;
+			};
+
+			// System messages
+			var notifyFilter = function() {
+				if(location.search.indexOf('filter') !== -1) {
+					if(button.is('.selected')) {
+						$('header .filtered').trigger('detach.notify');
+					}
+					else {
+						notifier.trigger('attach.notify', [Symphony.Language.get('You are looking at a filtered entry index.') + ' <a href="' + location.href.replace(location.search, '') + '">' + Symphony.Language.get('Clear?') + ' </a>', 'filtered']);
+					}
+				}
+			};
+
+			// API
+			return {
+				init: init,
+				clear: clear
+			}
+		}();
+
+		// Init filter interface
+		Publishfiltering.init();
 	});
 
 })(window.jQuery);
